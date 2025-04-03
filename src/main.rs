77fdd93b7e1eb984ced1cec156cdaa027c7fedcb
NithKon10xR <nithin.konda@10xr.co>
2025-04-03 22:36:11 +0530

    impl LB

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,14 @@ struct LoadBalancer {
     current_idx: usize,
 }
 
+impl LoadBalancer {
+    fn new(backends: Vec<String>) -> Self {
+        LoadBalancer {
+            backends,
+            current_idx: 0,
+        }
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
