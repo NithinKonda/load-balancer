b7886945b4b283ba21e4773ea0bce91ed6518cc8
NithKon10xR <nithin.konda@10xr.co>
2025-04-03 22:40:58 +0530

    impl the next backend

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,17 @@ impl LoadBalancer {
             current_idx: 0,
         }
     }
+
+    fn get_next_backend(&mut self) -> Option<String> {
+        if self.backends.is_empty() {
+            return None;
+        }
+
+        let backend = self.backends[self.current_idx].clone();
+        self.current_idx = (self.current_idx + 1) % self.backends.len();
+
+        Some(backend)
+    }
 }
 fn main() {
     println!("Hello, world!");
