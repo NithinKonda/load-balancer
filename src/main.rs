e814e2b25567452eb29fcbd82c65e4ed80056911
NithKon10xR <nithin.konda@10xr.co>
2025-04-03 22:33:37 +0530

    Create the LoadBalancer struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use log::{error, info, warn};
 use std::net::SocketAddr;
 use std::sync::Arc;
+use std::usize;
+use tokio::sync::Mutex;
+
+struct LoadBalancer {
+    backends: Vec<String>,
+    current_idx: usize,
+}
 
 fn main() {
     println!("Hello, world!");
