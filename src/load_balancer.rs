d8a1ecd76e54a23c19b3f6e56cdec6dc577c1b42
NithinKonda <nithinkonda142@gmail.com>
2025-04-11 22:08:58 +0530

    add the backend and sessionInfo enums

--- src/load_balancer.rs
+++ src/load_balancer.rs
@@ -6,3 +6,21 @@ use std::time::{Duration, Instant};
 use log::{info, warn};
 
 use crate::config::{LoadBalancerConfig, Strategy};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum HealthStatus {
+    Healthy,
+    Unhealthy(u32),
+}
+
+pub struct Backend {
+    pub url: String,
+    pub health_status: HealthStatus,
+    pub weight: u32,
+    pub current_weight: i32,
+}
+
+pub struct SessionInfo {
+    pub backend_url: String,
+    pub last_seen: Instant,
+}
